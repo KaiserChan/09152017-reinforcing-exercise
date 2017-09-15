1aca608730415536ea0ad26333c2c33eee2650bb
KaiserChan <chankaiserkk@gmail.com>
2017-09-15 18:36:38 -0400

    updated reinforing-exercise ... tightened up the codes, a little bit cleaner now

--- reinforcing-exercise.rb
+++ reinforcing-exercise.rb
@@ -2,25 +2,22 @@ array = [
   "Totam", "ut", "odit","quis", "Maiores", "unde", "EX", "EST", "corporis"
 ]
 
-def audit(array)
-  array.each do |entry|
-    indv_array = entry.split(//)
-
-    if indv_array.count > 4 && lowercase_check(entry)
-      puts "long and lowercase"
-    elsif lowercase_check(entry)
-      puts "lowercase"
-    elsif indv_array.count > 4
-      puts "long"
-    else
-      puts entry
-    end
-  end
-end
 
 def lowercase_check(word)
   word == word.downcase
 end
 
+array.each do |entry|
+  indv_array = entry.split(//)
 
-puts audit(array)
+  if indv_array.count > 4 && lowercase_check(entry)
+    puts "long and lowercase"
+  elsif lowercase_check(entry)
+    puts "lowercase"
+  elsif indv_array.count > 4
+    puts "long"
+  else
+    puts entry
+  end
+  puts
+end
